5cab23d121def3947e482bff62446448932e1c6b
Maxim Borbashov <maxim.borbashov@gmail.com>
2026-05-04 19:00:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+def has_digit_in_password(password):
+    return any(symbol.isdigit() for symbol in password)
+
+def get_password(password):
+    return any(not symbol.isdigit() and symbol.isalpha() for symbol in password)
+
+def get_lenght(password):
+    if len(password) > 12:
+        return True
+    
+def has_leters_in_password(password):
+    return any(symbol.isalpha() for symbol in password)
+        
+def has_high_leters_in_password(password):
+    return any(symbol.isupper() for symbol in password)
+    
+def main():
+
+    password = input("Введите пароль:")
+    score = 0
+
+    functions = [
+        has_leters_in_password(password),
+        get_password(password),
+        has_high_leters_in_password(password),
+        has_digit_in_password(password),
+        get_lenght(password)
+    ]
+
+    for function in functions:
+        if function:
+            score += 2
+
+    print(f"Рейтинг пароля:{score}")
+
+if __name__ == "__main__":
+    main()
